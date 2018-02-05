266833f3e03a751aa637032311b1107a3bf3ef23
DaCortex <manuelhartmann@outlook.com>
2018-02-05 21:07:50 +0100

    Initial commit for only webcam version.

--- stickpic.js
+++ stickpic.js
@@ -1,19 +1,5 @@
 var RESOLUTION = 20;
 var LINE_WIDTH = "1";
-var FILENAME = "laila.jpg"
-
-var img = new Image();
-img.src = FILENAME;
-var img_canvas = document.getElementById('image');
-img_canvas.width = img.width;
-img_canvas.height = img.height;
-
-var stickfigure_canvas = document.getElementById('stickfigure');
-stickfigure_canvas.width = img_canvas.width;
-stickfigure_canvas.height = img_canvas.height;
-
-var color = document.getElementById('color');
-
 
 function draw_line(context, start_x, start_y, end_x, end_y, color){
   context.beginPath();
@@ -82,130 +68,9 @@ function array_to_rgba(array){
              ', ' + array[2] + ', ' + (array[3] / 255) + ')';
 }
 
-function pick(event) {
-  var x = event.layerX;
-  var y = event.layerY;
-  var pixel = img_canvas.getContext('2d').getImageData(x, y, 1, 1);
-  var rgba = array_to_rgba(pixel.data);
-  color.style.background =  rgba;
-  color.textContent = rgba;
-}
-
-img.onload = function() {
-  img_canvas.getContext('2d').drawImage(img, 0, 0);
-  img.style.display = 'none';
-  image_to_stickpic(img_canvas,stickfigure_canvas);
-};
-
-img_canvas.addEventListener('mousemove', pick);
-
-var camera = (function() {
-  var options;
-  var video, canvas, context;
-  var renderTimer;
-
-  function initVideoStream() {
-    video = document.createElement("video");
-    video.setAttribute('width', options.width);
-    video.setAttribute('height', options.height);
-
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
-
-    if (navigator.getUserMedia) {
-      navigator.getUserMedia({
-        video: true
-      }, function(stream) {
-        options.onSuccess();
-
-        if (video.mozSrcObject !== undefined) { // hack for Firefox < 19
-          video.mozSrcObject = stream;
-        } else {
-          video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
-        }
-        
-        initCanvas();
-      }, options.onError);
-    } else {
-      options.onNotSupported();
-    }
-  }
-
-  function initCanvas() {
-    canvas = options.targetCanvas || document.createElement("canvas");
-    canvas.setAttribute('width', options.width);
-    canvas.setAttribute('height', options.height);
-
-    context = canvas.getContext('2d');
-
-    // mirror video
-    if (options.mirror) {
-      context.translate(canvas.width, 0);
-      context.scale(-1, 1);
-    }
-
-    startCapture();
-  }
-
-  function startCapture() {
-    video.play();
-
-    renderTimer = setInterval(function() {
-      try {
-        context.drawImage(video, 0, 0, video.width, video.height);
-        options.onFrame(canvas);
-      } catch (e) {
-        // TODO
-      }
-    }, Math.round(1000 / options.fps));
-  }
-
-  function stopCapture() {
-    pauseCapture();
-
-    if (video.mozSrcObject !== undefined) {
-      video.mozSrcObject = null;
-    } else {
-      video.src = "";
-    }
-  }
-
-  function pauseCapture() {
-    if (renderTimer) clearInterval(renderTimer);
-    video.pause();
-  }
-
-  return {
-    init: function(captureOptions) {
-      var doNothing = function(){};
-
-      options = captureOptions || {};
-
-      options.fps = options.fps || 30;
-      options.width = options.width || 640;
-      options.height = options.height || 480;
-      options.mirror = options.mirror || false;
-      options.targetCanvas = options.targetCanvas || null; // TODO: is the element actually a <canvas> ?
-
-      options.onSuccess = options.onSuccess || doNothing;
-      options.onError = options.onError || doNothing;
-      options.onNotSupported = options.onNotSupported || doNothing;
-      options.onFrame = options.onFrame || doNothing;
-
-      initVideoStream();
-    },
-
-    start: startCapture,
-
-    pause: pauseCapture,
-
-    stop: stopCapture
-  };
-})();
-
 camera.init({
-  width: 640, // default: 640
-  height: 480, // default: 480
+  width: 1280, // default: 640
+  height: 720, // default: 480
   fps: 30, // default: 30
   mirror: false,  // default: false
   targetCanvas: document.getElementById('webcam'), // default: null 
