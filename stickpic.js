f14d818fee1933ad036d73634af619f140d9f41f
DaCortex <manuelhartmann@outlook.com>
2018-02-05 15:35:07 +0100

    Add averaging of pixels.

--- stickpic.js
+++ stickpic.js
@@ -8,9 +8,9 @@ var stickfigure_canvas = document.getElementById('stickfigure');
 var color = document.getElementById('color');
 
 
-function pixel_to_rgba(pixel){
-    return 'rgba(' + pixel.data[0] + ', ' + pixel.data[1] +
-             ', ' + pixel.data[2] + ', ' + (pixel.data[3] / 255) + ')';
+function array_to_rgba(array){
+    return 'rgba(' + array[0] + ', ' + array[1] +
+             ', ' + array[2] + ', ' + (array[3] / 255) + ')';
 }
 
 function draw_line(canvas, start_x, start_y, end_x, end_y, color){
@@ -23,9 +23,9 @@ function draw_line(canvas, start_x, start_y, end_x, end_y, color){
   ctx.stroke();
 }
 
-function draw_stick_figure(canvas, pixel, x_pos, y_pos, size){
+function draw_stick_figure(canvas, color, x_pos, y_pos, size){
   ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, size, size);
+  ctx.clearRect(x_pos, y_pos, size, size);
   /*
   ctx.beginPath();
   ctx.lineWidth = "1";
@@ -37,9 +37,9 @@ function draw_stick_figure(canvas, pixel, x_pos, y_pos, size){
           2*Math.PI);
   ctx.stroke();
   */
-  var angle_red   = ((pixel.data[0]/255.0) * 120.0)       * (Math.PI / 180.0);
-  var angle_green = ((pixel.data[1]/255.0) * 120.0 + 120) * (Math.PI / 180.0);
-  var angle_blue  = ((pixel.data[2]/255.0) * 120.0 + 240) * (Math.PI / 180.0);
+  var angle_red   = ((color[0]/255.0) * 120.0)       * (Math.PI / 180.0);
+  var angle_green = ((color[1]/255.0) * 120.0 + 120) * (Math.PI / 180.0);
+  var angle_blue  = ((color[2]/255.0) * 120.0 + 240) * (Math.PI / 180.0);
 
   draw_line(canvas,
             x_pos + size*0.5,
@@ -68,10 +68,22 @@ function image_to_stickpic(image_canvas, stickpic_canvas){
 
   for (var x = 0; x < amount_horizontal; x++) {
     for (var y = 0; y < amount_vertical; y++) {
-      var pixel = img_canvas.getContext('2d').getImageData(x * RESOLUTION, y * RESOLUTION, 1, 1);
-      //average pixel data
-      var rgba = pixel_to_rgba(pixel);
-      draw_stick_figure(stickpic_canvas, pixel, x * RESOLUTION, y * RESOLUTION, RESOLUTION);
+      var image_data = img_canvas.getContext('2d').getImageData(x * RESOLUTION, y * RESOLUTION, RESOLUTION, RESOLUTION);
+
+      var average = [0,0,0,0];
+      for(var i = 0; i < image_data.data.length; i += 4)
+      {
+        average[0] += image_data.data[i];
+        average[1] += image_data.data[i+1];
+        average[2] += image_data.data[i+2];
+        average[3] += image_data.data[i+3];
+      }
+      average[0] = average[0] / (image_data.data.length / 4);
+      average[1] = average[1] / (image_data.data.length / 4);
+      average[2] = average[2] / (image_data.data.length / 4);
+      average[3] = average[3] / (image_data.data.length / 4);
+
+      draw_stick_figure(stickpic_canvas, average, x * RESOLUTION, y * RESOLUTION, RESOLUTION);
     }
   }
 }
@@ -80,7 +92,7 @@ function pick(event) {
   var x = event.layerX;
   var y = event.layerY;
   var pixel = img_canvas.getContext('2d').getImageData(x, y, 1, 1);
-  var rgba = pixel_to_rgba(pixel);
+  var rgba = array_to_rgba(pixel.data);
   color.style.background =  rgba;
   color.textContent = rgba;
   //draw_stick_figure(stickfigure_canvas, pixel, 0, 0, RESOLUTION);
