d840ba48ee14659de173794089542528667f70a7
DaCortex <manuelhartmann@outlook.com>
2018-02-05 17:22:58 +0100

    Update on dynamic setting of width and height.

--- stickpic.js
+++ stickpic.js
@@ -1,8 +1,9 @@
-var RESOLUTION = 10;
+var RESOLUTION = 15;
 var LINE_WIDTH = "1";
+var FILENAME = "laila.jpg"
 
 var img = new Image();
-img.src = 'RGB-space.jpg';
+img.src = FILENAME;
 var img_canvas = document.getElementById('image');
 img_canvas.width = img.width;
 img_canvas.height = img.height;
@@ -13,52 +14,34 @@ stickfigure_canvas.height = img_canvas.height;
 
 var color = document.getElementById('color');
 
-function array_to_rgba(array){
-    return 'rgba(' + array[0] + ', ' + array[1] +
-             ', ' + array[2] + ', ' + (array[3] / 255) + ')';
+function draw_line(context, start_x, start_y, end_x, end_y, color){
+  context.beginPath();
+  context.lineWidth = LINE_WIDTH;
+  context.strokeStyle = color;
+  context.moveTo(start_x, start_y);
+  context.lineTo(end_x, end_y);
+  context.stroke();
 }
 
-function draw_line(canvas, start_x, start_y, end_x, end_y, color){
-  ctx = canvas.getContext('2d');
-  ctx.beginPath();
-  ctx.lineWidth = LINE_WIDTH;
-  ctx.strokeStyle = color;
-  ctx.moveTo(start_x, start_y);
-  ctx.lineTo(end_x, end_y);
-  ctx.stroke();
-}
-
-function draw_stick_figure(canvas, color, x_pos, y_pos, size){
-  ctx = canvas.getContext('2d');
-  ctx.clearRect(x_pos, y_pos, size, size);
-  /*
-  ctx.beginPath();
-  ctx.lineWidth = "1";
-  ctx.strokeStyle = "black";
-  ctx.arc(x_pos + size*0.5,
-          y_pos + size*0.5,
-          size*0.5,
-          0,
-          2*Math.PI);
-  ctx.stroke();
-  */
+function draw_stick_figure(context, color, x_pos, y_pos, size){
+  context.clearRect(x_pos, y_pos, size, size);
   var angle_red   = ((color[0]/255.0) * 120.0)       * (Math.PI / 180.0);
   var angle_green = ((color[1]/255.0) * 120.0 + 120) * (Math.PI / 180.0);
   var angle_blue  = ((color[2]/255.0) * 120.0 + 240) * (Math.PI / 180.0);
 
-  draw_line(canvas,
+  draw_line(context,
             x_pos + size*0.5,
             y_pos + size*0.5,
             x_pos + size*0.5 + size*0.5 * Math.sin(angle_red),
             y_pos + size*0.5 + size*0.5 * Math.cos(angle_red),
             "red");
-  draw_line(canvas,
+  draw_line(context,
             x_pos + size*0.5,
             y_pos + size*0.5,
             x_pos + size*0.5 + size*0.5 * Math.sin(angle_green),
             y_pos + size*0.5 + size*0.5 * Math.cos(angle_green),
             "green");
-  draw_line(canvas,
+  draw_line(context,
             x_pos + size*0.5,
             y_pos + size*0.5,
             x_pos + size*0.5 + size*0.5 * Math.sin(angle_blue),
@@ -68,8 +51,8 @@ function draw_stick_figure(canvas, color, x_pos, y_pos, size){
 
 function image_to_stickpic(image_canvas, stickpic_canvas){
   
-  var amount_horizontal = Math.floor(800 / RESOLUTION);
-  var amount_vertical = Math.floor(449 / RESOLUTION);
+  var amount_horizontal = Math.floor(image_canvas.width / RESOLUTION);
+  var amount_vertical = Math.floor(image_canvas.height / RESOLUTION);
 
   for (var x = 0; x < amount_horizontal; x++) {
     for (var y = 0; y < amount_vertical; y++) {
@@ -88,9 +71,14 @@ function image_to_stickpic(image_canvas, stickpic_canvas){
       average[2] = average[2] / (image_data.data.length / 4);
       average[3] = average[3] / (image_data.data.length / 4);
 
-      draw_stick_figure(stickpic_canvas, average, x * RESOLUTION, y * RESOLUTION, RESOLUTION);
+      draw_stick_figure(stickpic_canvas.getContext('2d'), average, x * RESOLUTION, y * RESOLUTION, RESOLUTION);
     }
   }
+}
+
+function array_to_rgba(array){
+    return 'rgba(' + array[0] + ', ' + array[1] +
+             ', ' + array[2] + ', ' + (array[3] / 255) + ')';
 }
 
 function pick(event) {
@@ -100,7 +88,6 @@ function pick(event) {
   var rgba = array_to_rgba(pixel.data);
   color.style.background =  rgba;
   color.textContent = rgba;
-  //draw_stick_figure(stickfigure_canvas, pixel, 0, 0, RESOLUTION);
 }
 
 img.onload = function() {
